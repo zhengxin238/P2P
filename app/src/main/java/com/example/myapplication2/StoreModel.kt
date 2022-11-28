8688895034825b7166753b836310f75e1ee14b33
zhengxin238 <zhengxin238@gmail.com>
2022-11-28 16:56:04 +0100

    commit store function worked half

---
+++ app/src/main/java/com/example/myapplication2/StoreModel.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication2
+
+data class StoreModel(
+
+    val appTitle: String,
+    val averagePower: Int,
+    val endTime: String,
+    val energyConsumption: Int,
+    val startTime: String
+)
